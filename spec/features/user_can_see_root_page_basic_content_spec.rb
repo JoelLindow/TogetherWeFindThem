9e30e3caea242a63c1ccea75da78566fbacb4d42
JoelLindow <joellindow@gmail.com>
2017-11-16 15:45:07 -0700

    test passing. navbar basics plugged in.

--- spec/features/user_can_see_root_page_basic_content_spec.rb
+++ spec/features/user_can_see_root_page_basic_content_spec.rb
@@ -7,7 +7,7 @@ describe 'user visits root path' do
     # when I visit '/'
     visit '/'
     # I see a greeting and a mission statement
-    expect(page).to have_css('.greeting')
+    expect(page).to have_css('.greeting_root_block')
     # and I see a link to go to the list of missing persons (does not need to function in this story)
     expect(page).to have_css('.missing_persons_root_block')
     expect(page).to have_content("Continue as unregistered user")
@@ -15,8 +15,11 @@ describe 'user visits root path' do
     expect(page).to have_css('.visit_blog_root_block')
     expect(page).to have_content("Visit the Blog")
     # and I see an option to log in or create an account (does not need to function in this story)
+    expect(page).to have_css('.login_create_root_block')
     expect(page).to have_content("Login or Create Account")
     # And I see a button to 'log in' on the navbar (log in button is not required to function in this user story)
-    expect(page).to have_content("Login")
+    save_and_open_page
+
+    expect(page).to have_content("Members Login")
   end
 end
