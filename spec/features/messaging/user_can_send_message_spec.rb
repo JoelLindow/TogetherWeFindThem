fb04f9e06a7746a7e0564ab23fef2f3b4938e734
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-17 09:12:30 -0700

    wrote spec for starting users_controller resources

--- spec/features/messaging/user_can_send_message_spec.rb
+++ spec/features/messaging/user_can_send_message_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "user can send a message to another user" do
-  describe "user sees mailbox link in navbar" do
-    
+  describe "user navigates to other user's page" do
+    it "user sees user info" do
+      receipient = User.create(first_name: 'John', last_name: 'Doe', name: 'John Doe')
+
+      stub_omniauth
+
+      visit user_path(receipient)
+
+      expect(current_path).to eq("/users/#{receipient.id}")
+    end
   end
 end
